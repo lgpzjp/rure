65a1a0f8e942511196630d374bb1c9e776257d24
lgpzjp <93340606@qq.com>
2017-09-30 14:26:14 +0800

    web start

---
+++ rure-base/rure-base-common/src/main/java/com/bdth/rure/interceptor/WebInterceptor.java
@@ -0,0 +1,7 @@
+package com.bdth.rure.interceptor;
+
+/**
+ * Created by Administrator on 2017/9/30.
+ */
+public class WebInterceptor {
+}
---
+++ rure-base/rure-base-web/src/main/java/com/bdth/rure/base/controller/IndexController.java
@@ -0,0 +1,7 @@
+package com.bdth.rure.base.controller;
+
+/**
+ * Created by Administrator on 2017/9/30.
+ */
+public class IndexController {
+}
